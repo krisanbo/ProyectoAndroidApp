97a02fccfdd3c75732f2767a2808b0d7496bfedf
krisanbo <krisanbo@gmail.com>
2023-04-21 14:04:27 +0200

    segunda diseño

--- app/src/main/java/com/example/proyectoandroidapp/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroidapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText et_nombre;
     private Button bt_jugar;
     private TextView tv1,tv_nuestro;
-    ImageView iv_inicio;
 
 
     @Override
@@ -28,34 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         et_nombre = (EditText) findViewById(R.id.et_nombre);
         String nombre = et_nombre.getText().toString();
-        iv_inicio=(ImageView)findViewById(R.id.iv_inicio);
-
-
-
-        for (int i =1; i<=2;i++) {
-            int numero = (int) (Math.random() * 2 +1);
-
-            Integer [] imagenes = {R.drawable.image0, R.drawable.image2};
-
-
-            switch (numero){
-                case 1:
-                    numero= 0;
-                    iv_inicio.setImageResource(imagenes[0]);
-                    break;
-                case 2:
-                    numero= 1;
-                    iv_inicio.setImageResource(imagenes[1]);
-                    break;
-
-            }
-        }
 
 
 
     }
-
-
     //metodo boton jugar
 
     public void jugar(View view){
@@ -66,12 +40,15 @@ public class MainActivity extends AppCompatActivity {
 
         }else{
             Intent jugar= new Intent(this, com.example.proyectoandroidapp.Primernivel.class);
+            jugar.putExtra("nombre",et_nombre.getText().toString());
             startActivity(jugar);
-            Toast.makeText(this, "debes introducir tu nombre", Toast.LENGTH_SHORT).show();
+
 
 
         }
     }
+
+
 
 
 }
--- app/src/main/java/com/example/proyectoandroidapp/Primernivel.java
+++ app/src/main/java/com/example/proyectoandroidapp/Primernivel.java
@@ -3,12 +3,64 @@ package com.example.proyectoandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Primernivel extends AppCompatActivity {
+
+    private TextView tv_nombre, tv_score;
+    private RadioButton bt_opcion1, bt_opcion2, bt_opcion3;
+    private ImageView banderas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primernivel);
+
+        tv_nombre = (TextView) findViewById(R.id.tv_nombre);
+        bt_opcion1=(RadioButton)findViewById(R.id.boton_opcion1);
+        bt_opcion2=(RadioButton)findViewById(R.id.bt_opcion2);
+        bt_opcion3=(RadioButton)findViewById(R.id.bt_opcion3) ;
+        banderas=(ImageView)findViewById(R.id.banderas);
+
+        String viajero = getIntent().getStringExtra("nombre");
+        tv_nombre.setText(viajero);
+
+
+
     }
+    public void banderas(View view){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
