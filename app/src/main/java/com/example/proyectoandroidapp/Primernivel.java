790ecaed305c00142855a4336fd6425ed324be7c
Cristina <krisanbo@gmail.com>
2023-04-21 20:42:49 +0200

    modifcado imagen y boton aleatorio

--- app/src/main/java/com/example/proyectoandroidapp/Primernivel.java
+++ app/src/main/java/com/example/proyectoandroidapp/Primernivel.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,38 +30,45 @@ public class Primernivel extends AppCompatActivity {
         bt_opcion2=(RadioButton)findViewById(R.id.bt_opcion2);
         bt_opcion3=(RadioButton)findViewById(R.id.bt_opcion3) ;
         banderas=(ImageView)findViewById(R.id.banderas);
-
+// traer el nombre de la activity principal
         String viajero = getIntent().getStringExtra("nombre");
         tv_nombre.setText(viajero);
+// hacer muestre imagen aleatoria de banderas
+
+        int bandera= (int) (Math.random()*3);
+        if (bandera==0)  {
+            banderas.setImageResource(R.drawable.activityafrica);
+        }
+        if (bandera==1) {
+            banderas.setImageResource(R.drawable.activityamerica);
+        }
+        if (bandera==2) {
+            banderas.setImageResource(R.drawable.activityeuropa);
+        }
+
+   //hacer botones radiobutton aleatorios
+
+        int opciones= (int) (Math.random()*3);
+
+        if (opciones==0){
+            bt_opcion1.setText("españa");
+            bt_opcion2.setText("alemania");
+            bt_opcion3.setText("francia");
+        }
+        if (opciones==1) {
+            bt_opcion1.setText("alemania");
+            bt_opcion2.setText("españa");
+            bt_opcion3.setText("francia");
+        }
+        if (opciones==2){
+            bt_opcion1.setText("francia");
+            bt_opcion2.setText("alemania");
+            bt_opcion3.setText("españa");
+        }
+        // unir los 2
+
 
 
 
     }
-    public void banderas(View view){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
 }
